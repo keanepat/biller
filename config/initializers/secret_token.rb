9e34214e8148d5b41895c116d1c004165a4f4084
Patrick Keane <keanepat@Patricks-MacBook-Air.local>
2014-05-28 23:41:40 -0400

    continuing the tutorial for ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biller::Application.config.secret_key_base = '14527f7471cf44a4504b9aec3381ad0533592ce80ade09f0656121971ab679699ecaaccff927fe5955680d57164d46a9da85eb0c7b555a86d564ce8e4c010421'

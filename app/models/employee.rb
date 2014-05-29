3e7c2b486a432e543bbed61ffc3cedb341284d95
Patrick Keane <keanepat@Patricks-MacBook-Air.local>
2014-05-29 16:31:10 -0400

    working on has_and_belongs_to_many

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,6 @@
 class Employee < Account
 	has_many :time_entries
+	has_and_belongs_to_many :projects, :join_table => "employees_projects"
 
 	def total_hours
 		time_entries.sum(:time)
@@ -8,5 +9,5 @@ class Employee < Account
 	def log_time(amount, customer)
 		TimeEntry.create(time: amount, employee: self, account: customer)
 	end
-	
+
 end
